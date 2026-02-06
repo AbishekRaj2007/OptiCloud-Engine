8c4b74a52002acdb1df1411d91070711203f6f1e
Abishek Raj R R <abishekrajrr.cse2024@citchennai.net>
2026-02-06 09:48:26 +0530

    Worked on backend to add superuser

--- OptiCloud/optimization/models.py
+++ OptiCloud/optimization/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ResourceUsage(models.Model):
+    resource_id = models.CharField(max_length=100)
+    cpu_usage = models.FloatField()
+    memory_usage = models.FloatField()
+    cost = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.resource_id
